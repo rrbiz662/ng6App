1fb7f81b0a1f207186b9e990543202f0d18e5110
rrbiz662 <rrbiz662@gmail.com>
2019-01-15 15:57:11 -0600

    bind database data to UsersComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { UsersComponent } from './users/users.component';
 import { PostsComponent } from './posts/posts.component';
 import { UserComponent } from './user/user.component';
+// Need to import to inject into DataService.
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { UserComponent } from './user/user.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service'
 
 @Component({
   selector: 'app-users',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  private users: object;
+
+  constructor(private service: DataService) { }
 
   ngOnInit() {
+    this.service.getUsers().subscribe(
+      // Bind this.users to service user data.
+      service => this.users = service
+    )
   }
 
 }
